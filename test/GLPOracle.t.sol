128f8ae937569d9e97c3b7a0d7e46d56f7803781
Shan-YYYYY <2547953859@qq.com>
2023-07-25 18:27:34 +0800

    add GLPOrcaleTest

---
+++ test/GLPOracle.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "../contracts/oracles/GLPOracle.sol";
+import "forge-std/Test.sol";
+
+
+contract GLPOracleTest is GLPOracle{
+
+    GLPOracle glpOralce;
+
+    function setUp() public {
+        glpOralce = new GLPOracle;
+
+    }
+}
