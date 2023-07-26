7ef5b633268cc10526e3c91942bb11b1001242a8
雍周文 <2547953859@qq.com>
2023-07-27 00:04:05 +0800

    GLPOW update

--- test/GLPOracleWrapper.t.sol
+++ test/GLPOracleWrapper.t.sol
@@ -4,6 +4,22 @@ pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
 
-contract GLPOracleWrapperTest is Test{
-    
+import "../contracts/interfaces/IOracleWrapper.sol";
+import "../contracts/oracles/GLPOracle.sol";
+import "../contracts/wrappers/GLPOracleWrapper.sol";
+
+contract GLPOracleWrapperTest is Test {
+    GLPOracle glpOracle;
+    GLPOracleWrapper glpOracleWrapper;
+
+    function setUp() public {
+        glpOracle = new GLPOracle();
+        glpOracleWrapper = new GLPOracleWrapper(address(glpOracle));
+    }
+
+    function test_GLPW__DEPLOYMENT() public {
+        IOracleWrapper.SavedResponse memory savedResponse;
+        savedResponse = glpOracleWrapper.savedResponse;
+        assertEq(savedResponse.lastPrice, 0);
+    }
 }
--- test/XDCOracle.t.sol
+++ test/XDCOracle.t.sol
@@ -1,15 +1,15 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.17;
+// pragma solidity 0.8.17;
 
-import "forge-std/Test.sol";
+// import "forge-std/Test.sol";
 
-import "../contract/oracles/XDCOracle.sol";
+// import "../contracts/oracles/XDCOracle.sol";
 
-contract XDCOracleTest is Test{
-    XDCOracle xdcOracle;
+// contract XDCOracleTest is Test {
+//     XDCOracle xdcOracle;
 
-    function setUp() public {
-        xdcOracle = new XDCOracle();
-    }
-}
+//     function setUp() public {
+//         xdcOracle = new XDCOracle();
+//     }
+// }
